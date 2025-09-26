25a560b5f7e17c46531d272b046dfd6fa733e39c
VanshPatel16 <vanshpate8999@gmail.com>
2025-09-26 15:57:53 +0530

    Add probability distribution for responses

--- Week_2/dual_LLM_generator.py
+++ Week_2/dual_LLM_generator.py
@@ -2,6 +2,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os,sys,time
 import yaml
+import random
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
@@ -22,6 +23,7 @@ def load_config(filepath="dual_LLM_gen_prompts.yaml"):
 config = load_config()
 personas = config.get("personas",{})
 prompt_templates = config.get("prompt_templates",{})
+interviewee_prompt_templates = config.get("interviewee_prompt_templates",{})
 
 def get_persona_prompts()->tuple | None :
     """Loads persona prompts from YAML file"""
@@ -47,50 +49,74 @@ interviewer = genai.GenerativeModel(model_name="gemini-2.5-flash",system_instruc
 interviewee = genai.GenerativeModel(model_name="gemini-2.5-flash",system_instruction=INTERVIEWEE_PERSONA)
 
 INITIAL_PROMPT = "TOPIC : Design a url shortening service like TinyURL"
-MAX_TURNS = 4
+MAX_TURNS = 5
 conversation_history = [INITIAL_PROMPT]
+interviewee_response_types = list(interviewee_prompt_templates["EASY"].keys())
+# print(interviewee_response_types)
 
-
-def next_prompt(role: str,last: bool) -> str:
+def next_prompt(role: str,last: bool,question_difficulty="") -> str:
     if role == "Interviewer":
         if not last:
-            next_prompt_template = prompt_templates.get("reply_prompt",{}).get("template")
-            return next_prompt_template.format(
-                role=role,
-            )
+            next_prompt_template = prompt_templates.get("interviewer_reply_prompt",{}).get("template")
+            return next_prompt_template
         else:
-            concluding_template = prompt_templates.get("conclude_prompt",{}).get("template")
+            concluding_template = prompt_templates.get("interviewer_conclude_prompt",{}).get("template")
             return concluding_template
     else:
-        
-        next_prompt_template = prompt_templates.get("reply_prompt",{}).get("template")
-        return next_prompt_template.format(
-            role=role,
-        )
+        if not last:
+            styles_config = config["interviewee_prompt_templates"][question_difficulty]
+            style_weights = [style['weight'] for style in styles_config.values()]
+            type_of_prompt = random.choices(interviewee_response_types,weights=style_weights,k=1)[0]
+            next_prompt_template = styles_config[type_of_prompt]['template']
+            prompt = f"TYPE : {type_of_prompt}\n"
+            prompt += next_prompt_template
+            return prompt
+        else:
+            concluding_template = prompt_templates.get("interviewee_conclude_prompt",{}).get("template")
+
+            return concluding_template
         
 
+question_difficulty = "EASY"
+
 for iter in range(2 * MAX_TURNS):
-    is_last = (iter == 2 * (MAX_TURNS - 1))
+
+    is_last = (iter >= 2 * (MAX_TURNS - 1))
 
     prompt = ""
     response = ""
+    response_txt = ""
     if iter % 2 == 0:
         # Interviewer's turn.
         prompt = next_prompt("Interviewer",is_last)
         history = conversation_history
         history.append(prompt)
         response = interviewer.generate_content(history)
+        if "<CONFUSING>" in response.text:
+            question_difficulty = "CONFUSING"
+            response_txt = response.text.replace("<CONFUSING>","").strip()
+        elif "<EASY>" in response.text:
+            question_difficulty = "EASY"
+            response_txt = response.text.replace("<EASY>","").strip()
+        elif not is_last:
+            print("No difficulty assigned.")
+            question_difficulty = "EASY"
+            sys.exit(1)
+        else:
+            response_txt = response.text
 
     else:
         # Interviewee's turn.
-        prompt = next_prompt("Interviewee",is_last)
+        prompt = next_prompt("Interviewee",is_last,question_difficulty)
         history = conversation_history
         history.append(prompt)
         response = interviewee.generate_content(history)
+        response_txt = response.text
     
-    conversation_history.append(response.text)
-    # print('\nPrompt: ' + prompt + '\n')
+    conversation_history.append(response_txt)
+    # print(conversation_history)
+    # print("\nprompt: " + prompt + '\n')
     print('\n' + response.text + '\n')
-    time.sleep(1)
+    time.sleep(2)
     
 
